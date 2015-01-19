edd945b5c4afd4a9bec3ee06503c1aed370cee2e
Liang.Fang <liang.fang@tpooo.com>
2015-01-19 18:31:23 +0800

    aa

--- bbsAnimate/js/app.js
+++ bbsAnimate/js/app.js
@@ -16,7 +16,7 @@ app.controller('MainController', function($scope, $http, $timeout, $filter){
       $scope.show_page_post = false;
       $scope.show_page_comment = false;
       $scope.page_nav = null;
-      // 前进
+
       $scope.postList2post = function(post){
         $scope.show_page_post_list = false;
         $scope.show_page_post = true;
@@ -24,17 +24,48 @@ app.controller('MainController', function($scope, $http, $timeout, $filter){
 
         $http.get("json/post.json")
           .success(function(response){
-            $timeout(function(){              
-              $scope.post = response; 
-              $scope.post.postTitle = post.postTitle;
+            $timeout(function(){
+              $scope.post = response;
+              //$scope.post.fullContent = response.postContent;
+
+              debugger;
+              if(response.postContent.length > 80){
+                //$scope.post.postContent = response.postContent.substring(0,80) + " ...";
+                $scope.postContentCollapse = true;
+                $scope.postContentCollapseText = "展开";
+              }
+      
               $scope.post.headPortraitUrl = post.headPortraitUrl;
               $scope.post.writerName = post.writerName;
               $scope.post.postTime = post.postTime;
               $scope.loading_detail = false;
-            }, 0);      
+            }, 20);      
           });
       }
-      // 前进
+
+      $scope.showFullPost = function(){
+        // 展开
+        if($scope.postContentCollapse === true){
+          //$scope.post.postContent = $scope.post.fullContent;
+          $scope.postContentCollapse = false;
+          $scope.fullContent = "full-content";
+          $timeout(function(){
+            
+          }, 10)
+        }
+        // 折叠
+        else if($scope.postContentCollapse === false){
+          //$scope.post.postContent = $scope.post.fullContent.substring(0,80);
+          $scope.postContentCollapse = true;
+          $scope.fullContent = null;
+          $timeout(function(){
+            
+          }, 10)
+        }
+
+        
+      }
+
       $scope.post2comment = function(comment){        
         $scope.show_page_post = false;
         $scope.show_page_comment = true;
@@ -42,7 +73,7 @@ app.controller('MainController', function($scope, $http, $timeout, $filter){
 
         $scope.comment = comment;
       }
-      // 返回
+
       $scope.back = function(){        
         if($scope.show_page_post == true){
           $scope.show_page_post = false;
@@ -53,7 +84,8 @@ app.controller('MainController', function($scope, $http, $timeout, $filter){
           $scope.show_page_comment = false;
           $scope.show_page_post = true;
         }
-      }      
+      }    
+       
 
       $http.get("json/list.json")
         .success(function(response){
