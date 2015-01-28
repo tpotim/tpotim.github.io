a3e2747438a5001ae7cae98c159f849806023b04
Liang.Fang <liang.fang@tpooo.com>
2015-01-28 17:46:31 +0800

    ccccc

--- new_lessjs/new/scripts/controllers/detail.js
+++
@@ -1,6 +0,0 @@
-var DetailModule = angular.module('detailModule', []);
-
-DetailModule.controller('detailCtrl', ['$routeParams', '$scope', function($routeParams, $scope) {
-  $scope.Id = $routeParams;
-
-}]);
