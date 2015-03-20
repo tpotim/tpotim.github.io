6852b948fa6a7bcaa99b291315c428692e10c073
Liang.Fang <liang.fang@tpooo.com>
2015-03-20 18:33:41 +0800

    aaa

--- H5html-invitation/js/commer.js
+++ H5html-invitation/js/commer.js
@@ -10,6 +10,9 @@ $(document).ready(function(){
     	$(".page-fix").animate({top:23+"%"}, 3000,function(){
             $(".page-loading").fadeOut(1500);
         })
-    })
+    });
+
+    $('.text-img-wrap .left-img').removeClass('img-hide').addClass('animated slideInDown');
+    $('.text-img-wrap .right-img').removeClass('img-hide').addClass('animated slideInUp');
 
 });
